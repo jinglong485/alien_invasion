065f61402c8688664033dbb9bb7426be2af6b498
jinglong485 <guojinglong1992@gmail.com>
2024-05-05 13:02:36 -0700

    refactor chekc events and add Q to quit game

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,21 +30,38 @@ class AlienInvasion:
 	def _check_events(self):
 		"""Respond to keypresses and mouse events"""
 		for event in pygame.event.get():
+				#print(event)
 				if event.type == pygame.QUIT:
 					sys.exit()
 				elif event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_RIGHT:
-						# move the ship to the right
-						#self.ship.rect.x += 1
-						self.ship.moving_right = True
-					elif event.key == pygame.K_LEFT:
-						self.ship.moving_left = True
-
+					self._check_keydown_events(event)
 				elif event.type == pygame.KEYUP:
-					if event.key == pygame.K_RIGHT:
-						self.ship.moving_right = False
-					elif event.key == pygame.K_LEFT:
-						self.ship.moving_left = False
+					self._check_keyup_events(event)
+				'''elif event.key == pygame.K_Q:
+					sys.exit()
+				above is wrong, since only event that relates to key has key
+				attirbutes, e.g. mouse movements and clicks are also events,
+				but they don't have key as attribute. so to check if q is
+				pressed, it should be in _check_keydown_events() 
+				'''
+
+	def _check_keydown_events(self, event):
+		"""Respond to keypresses"""
+		if event.key == pygame.K_RIGHT:
+			# move the ship to the right
+			#self.ship.rect.x += 1
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+		elif event.key == pygame.K_q:
+			sys.exit()
+
+	def _check_keyup_events(self, event):
+		"""Respond to key releases"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
 
 	def _update_screen(self):
 		"""update images on the screen, and flip to the new screen"""
