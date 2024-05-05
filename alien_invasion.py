3b400c353aa077238de0c443f4e5e49c98246820
jinglong485 <guojinglong1992@gmail.com>
2024-05-05 14:24:03 -0700

    add bullet and related settings, update methods and control. add comments for possible bullet behavier change.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -22,6 +23,7 @@ class AlienInvasion:
 		#Set the background color. Color is 8bit-RGB
 		#set the ship
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 	def run_game(self):
 		"""Start the main loop for the game."""
@@ -29,6 +31,18 @@ class AlienInvasion:
 			#Watch for keyboard and mouse events.
 			self._check_events()
 			self.ship.update()
+			self.bullets.update()
+			#python expect the list stays the same length as the loop is runing
+			#copy of the list for the loop is necessary
+			for bullet in self.bullets.copy():
+				if bullet.rect.bottom <= 0:
+					self.bullets.remove(bullet)
+			#the commented chaged bullet behavier, instead stop firing, the
+			# eralies fired bullet disaprears. make the total bullets less
+			# than self.setting.bullet_allowed
+			#if len(self.bullets) > self.settings.bullet_allowed:
+			#	self.bullets.remove(self.bullets.sprites().pop(0))
+			#print(len(self.bullets))
 			self._update_screen()
 			self.clock.tick(60)
 
@@ -60,6 +74,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self, event):
 		"""Respond to key releases"""
@@ -72,7 +88,15 @@ class AlienInvasion:
 		"""update images on the screen, and flip to the new screen"""
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet() 
 		pygame.display.flip()
+
+	def _fire_bullet(self):
+		"""Create a new bullet and add it to the bullets group"""
+		if len(self.bullets) < self.settings.bullet_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -8,4 +8,10 @@ class Settings:
 		self.screen_height = 800
 		# I like Blue sky for Haiku
 		self.bg_color = (0, 128, 220)
-		self.ship_speed = 1.5
+		self.ship_speed = 5
+		# Bullet settings
+		self.bullet_speed = 10.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullet_allowed = 3
